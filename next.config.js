5e5c876c6bce5a6b864ccccbe3f221aa010eda3b
Beto Toro <beto.toro@zemoga.com>
2022-02-14 14:07:50 -0500

    [FEAT]: finish login page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
+
+// SVGR config
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
+  }
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
-import { Text, Container, Spacer, Button } from '@nextui-org/react';
+import { Text, Container, Spacer, Button, Modal, Row, Checkbox, Input } from '@nextui-org/react';
+import Github from '../public/github.svg'
 
 
 const login: NextPage = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+  const handleModal = () => setIsVisible(!isVisible)
   return (
     <>
       <Container>
@@ -17,7 +21,27 @@ const login: NextPage = () => {
       </Container>
       <Container display='flex' direction='column' alignContent="flex-end">
         <Spacer y={15}></Spacer>
-        <Button size="sm">Log in</Button>
+        <Button size="sm" onClick={handleModal}>Log in</Button>
+         <Modal
+          closeButton
+          aria-labelledby="modal-title"
+          width='20rem'
+          open={isVisible}
+          onClose={handleModal}
+        >
+          <Modal.Header>
+              <Text id="modal-title" size={18} b>
+              Login
+              </Text>
+          </Modal.Header>
+            <Modal.Footer justify='center'>
+              <Button css={{backgroundColor: 'black'}}>
+                Login with Github
+                <Spacer x={0.5}/>
+                <Github />
+              </Button>
+            </Modal.Footer>
+        </Modal>
       </Container>
     </>
   )
