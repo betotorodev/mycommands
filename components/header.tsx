74fb37c6f5a1033e28a5095dd3f47f731a089ebb
Beto Toro <beto.toro@zemoga.com>
2022-02-18 10:17:15 -0500

    [FEAT]: create the categories card and its show logic

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,17 @@
-import { Container, Grid, Input } from '@nextui-org/react'
-import { Search, ArrowDown } from 'iconoir-react'
+import { useState } from 'react'
+import { Card, Col, Container, Grid, Input } from '@nextui-org/react'
+import { Search, ArrowDown, ArrowUp } from 'iconoir-react'
 
 export const Header = () => {
+  const [toggle, setToggle] = useState(false)
+  const handleModal = () => setToggle(!toggle)
   return (
     <Container>
       <Grid.Container gap={1}>
         <Grid xs={8}>
             <Input size="md" width='100%' clearable placeholder="busca tu comando" contentRight={<Search />} />
         </Grid>
-        <Grid xs={4}>
+        <Grid xs={4} css={{position: 'relative'}}>
           <div style={{
             display: 'flex',
             alignItems: 'center',
@@ -19,8 +22,19 @@ export const Header = () => {
             backgroundColor: '#f4f4f4',
           }}>
             all
-            <ArrowDown />
+            {toggle && <ArrowUp onClick={handleModal} />}
+            {!toggle && <ArrowDown onClick={handleModal} />}
           </div>
+          {
+            toggle && 
+              <Card css={{position: 'absolute', bottom: '-124px', right: '6px', zIndex: '1', backgroundColor: '#f4f4f4'}}>
+                <ul style={{margin: '0', textAlign: 'center'}}>
+                  <li>Git</li>
+                  <li>Docker</li>
+                  <li>Npm</li>
+                </ul>
+              </Card>
+          }
         </Grid>
       </Grid.Container>
     </Container>
