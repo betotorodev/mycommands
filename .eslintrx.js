e86f8d8e3775f6875bc98c8d8214a1f71837696f
Beto Toro <beto.toro@zemoga.com>
2022-02-19 18:45:09 -0500

    [FEAT]: add eslintrc.js config file

---
+++ .eslintrx.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'next']
+}
--- components/listOfCategories.tsx
+++ components/listOfCategories.tsx
@@ -2,8 +2,8 @@ import { Card } from '@nextui-org/react'
 
 export const ListOfCategories = () => {
   return (
-    <Card css={{position: 'absolute', bottom: '-124px', right: '6px', zIndex: '1', backgroundColor: '#f4f4f4'}}>
-      <ul style={{margin: '0', textAlign: 'center'}}>
+    <Card css={{ position: 'absolute', bottom: '-124px', right: '6px', zIndex: '1', backgroundColor: '#f4f4f4' }}>
+      <ul style={{ margin: '0', textAlign: 'center' }}>
         <li>Git</li>
         <li>Docker</li>
         <li>Npm</li>
