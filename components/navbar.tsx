5f604db40eb904da7c3da780151f1bf9cc7e5203
Beto Toro <beto.toro@zemoga.com>
2022-02-21 06:22:55 -0500

    [FEAT]: add routing into a navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useToggle } from 'hooks/useToggle';
 import { Container, Row, Avatar, Card} from '@nextui-org/react'
 import { List, AddCircledOutline } from 'iconoir-react';
@@ -9,8 +10,8 @@ export const Navbar = () => {
     <Container css={{position: 'fixed', bottom: '3rem'}}>
       <Card>
         <Row justify="space-between" align="center">
-          <List />
-          <AddCircledOutline height={36} width={36}/>
+          <Link href='/list' passHref><List /></Link>
+          <Link href='/add' passHref><AddCircledOutline height={36} width={36}/></Link>
           <Avatar onClick={setIsVisible} bordered rounded text="Beto" size="sm" color="primary" />
         </Row>
       </Card>
