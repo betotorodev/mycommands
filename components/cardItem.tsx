9af5660e617a9fcb33553bbed71d1c099fa06fa5
Beto Toro <beto.toro@zemoga.com>
2022-02-22 06:45:08 -0500

    [REFACTOR]: eliminate colummns container in categoryItem

--- components/cardItem.tsx
+++ components/cardItem.tsx
@@ -1,4 +1,4 @@
-import { useToggle } from 'hooks/useToggle';
+import { useToggle } from 'hooks/useToggle'
 import { Card, Col, Modal, Row, Text } from '@nextui-org/react'
 
 const themeColors = {
@@ -8,56 +8,67 @@ const themeColors = {
   textLight: '#E7ECED'
 }
 
-export const CardItem = ({ command }: {command: string}) => {
+export const CardItem = ({ command }: { command: string }) => {
   const [isVisible, handleModal] = useToggle()
   return (
-    <Card shadow={false} hoverable css={{backgroundColor: themeColors.backgroundLight, color: themeColors.textDark, fontFamily: 'mono'}}>
-      <Text css={{fontFamily: 'mono'}}>{command}</Text>
+    <Card
+      shadow={false}
+      hoverable
+      css={{
+        backgroundColor: themeColors.backgroundLight,
+        color: themeColors.textDark,
+        fontFamily: 'mono'
+      }}
+    >
+      <Text css={{ fontFamily: 'mono' }}>{command}</Text>
       <Card.Footer>
-        <Row>
-          <Col>
-            <small style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
-                  width: 'fit-content',
-                  padding: ' 8px 16px',
-                  borderRadius: '0.75rem',
-                  backgroundColor: '#7928ca',
-                  color: 'white'
-                }}>
-                  Git
-            </small>
-          </Col>
-          <Col>
-            <small onClick={handleModal} style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
-                  width: 'fit-content',
-                  padding: ' 8px 16px',
-                  borderRadius: '0.75rem',
-                  backgroundColor: '#444',
-                  color: 'white'
-                }}>
-                  Info
-            </small>
-              <Modal
-                scroll
-                closeButton
-                aria-labelledby="modal-title"
-                width='20rem'
-                open={isVisible}
-                onClose={handleModal}
-                style={{paddingBottom: '1.5rem'}}
-              >
-                <Modal.Body>
-                  <Text id="modal-title" size={18} b>
-                    Este comando me permite guardar los cambios en stash y poder cambiar de rama sin tener que hacer commits innecesarios
-                  </Text>
-                </Modal.Body>
-            </Modal>
-          </Col>
+        <Row justify="flex-start">
+          <small
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              width: 'fit-content',
+              padding: ' 8px 16px',
+              marginRight: '8px',
+              borderRadius: '0.75rem',
+              backgroundColor: '#7928ca',
+              color: 'white'
+            }}
+          >
+            Git
+          </small>
+          <small
+            onClick={handleModal}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              width: 'fit-content',
+              padding: ' 8px 16px',
+              borderRadius: '0.75rem',
+              backgroundColor: '#444',
+              color: 'white'
+            }}
+          >
+            Info
+          </small>
+          <Modal
+            scroll
+            closeButton
+            aria-labelledby='modal-title'
+            width='20rem'
+            open={isVisible}
+            onClose={handleModal}
+            style={{ paddingBottom: '1.5rem' }}
+          >
+            <Modal.Body>
+              <Text id='modal-title' size={18} b>
+                Este comando me permite guardar los cambios en stash y poder
+                cambiar de rama sin tener que hacer commits innecesarios
+              </Text>
+            </Modal.Body>
+          </Modal>
         </Row>
       </Card.Footer>
     </Card>
