8d89deb1dcd4f0829255abd7a861bf5eddcb3832
Beto Toro <beto.toro@zemoga.com>
2022-02-22 06:48:18 -0500

    [FEAT]: finish ipad responsive

--- components/listOfCommands.tsx
+++ components/listOfCommands.tsx
@@ -4,11 +4,12 @@ import { CardItem } from 'components/cardItem'
 export const ListOfCommands = () => {
   return (
     <Container>
-      <Grid.Container gap={1} css={{marginBottom: '8rem'}}>
+      <Grid.Container gap={1} css={{ marginBottom: '8rem' }}>
         {
           [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(i => (
             <Grid key={i} xs={6}>
-              <CardItem command='Git push texto de prueba pa ver esta chimbá'/>
+              <CardItem command='Git push texto de prueba pa ver esta el largo del componente
+              '/>
             </Grid>
           ))
         }
