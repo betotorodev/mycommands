09e3599ce98ee852f72e5c0a034db00428c7fa32
Beto Toro <beto.toro@zemoga.com>
2022-02-22 12:12:51 -0500

    [FEAT]: add the useMediaQuery custom hook

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,46 @@
 import Link from 'next/link'
-import { useToggle } from 'hooks/useToggle';
-import { Container, Row, Avatar, Card} from '@nextui-org/react'
-import { List, AddCircledOutline } from 'iconoir-react';
+import { useToggle } from 'hooks/useToggle'
+import { Container, Row, Avatar, Card } from '@nextui-org/react'
+import { List, AddCircledOutline } from 'iconoir-react'
 import { CloseSession } from 'components/closeSession'
+import { useLargeBreakpoint } from 'hooks'
 
 export const Navbar = () => {
   const [isVisible, setIsVisible] = useToggle()
+  const isDesktop = useLargeBreakpoint()
+  console.log(isDesktop)
   return (
-    <Container css={{
-        position: 'fixed', 
+    <Container
+      css={{
+        position: 'fixed',
         bottom: '3rem',
         maxWidth: '400px',
-      }}>
+        '@lg': {
+          left: '3rem',
+          bottom: '50%',
+          transform: 'rotate(90deg)'
+        }
+      }}
+    >
       <Card>
-        <Row justify="space-between" align="center">
-          <Link href='/list' passHref><List /></Link>
-          <Link href='/add' passHref><AddCircledOutline height={36} width={36}/></Link>
-          <Avatar onClick={setIsVisible} bordered rounded text="Beto" size="sm" color="primary" />
+        <Row justify='space-between' align='center'>
+          <Link href='/list' passHref>
+            <List style={{ transform: `${isDesktop ? 'rotate(270deg)' : ''}` }} />
+          </Link>
+          <Link href='/add' passHref>
+            <AddCircledOutline height={36} width={36} />
+          </Link>
+          <Avatar
+            onClick={setIsVisible}
+            bordered
+            rounded
+            text='Beto'
+            size='sm'
+            color='primary'
+          />
         </Row>
       </Card>
       {isVisible && <CloseSession />}
     </Container>
   )
-} 
+}
---
+++ hooks/useMediaQuery.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+export enum Breakpoints {
+  extraSmall = '(max-width: 650px)',
+  small = '(min-width: 650px)',
+  medium = '(min-width: 960px)',
+  large = '(min-width: 1280px)',
+  xLarge = '(min-width: 1400px)',
+}
+
+export const useMediaQuery = (query: string): boolean => {
+  let mediaMatch: MediaQueryList
+  const [matches, setMatches] = useState(false)
+
+  useEffect(() => {
+    mediaMatch = window.matchMedia(query)
+    const handler = (e: MediaQueryListEvent) => setMatches(e.matches)
+    mediaMatch.addEventListener('change', handler)
+    return () => mediaMatch.removeEventListener('change', handler)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return matches
+};
+
+export const useXXLargeBreakpoint = () => useMediaQuery(Breakpoints.extraSmall)
+export const useSmallBreakpoint = () => useMediaQuery(Breakpoints.small)
+export const useMediumBreakpoint = () => useMediaQuery(Breakpoints.medium)
+export const useLargeBreakpoint = () => useMediaQuery(Breakpoints.large)
+export const useXLargeBreakpoint = () => useMediaQuery(Breakpoints.xLarge)
