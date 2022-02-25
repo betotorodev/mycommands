2cd25e4ea1e20d839ca6c9d94be9ea201d68386d
Beto Toro <beto.toro@zemoga.com>
2022-02-25 06:21:18 -0500

    [STYLE]: change the background image for a custom pattern

--- components/listOfCommands.tsx
+++ components/listOfCommands.tsx
@@ -2,17 +2,21 @@ import { Container, Grid } from '@nextui-org/react'
 import { CardItem } from 'components/cardItem'
 
 export const ListOfCommands = () => {
+  const array = Array(15).fill('')
   return (
     <Container>
-      <Grid.Container gap={1} css={{ marginBottom: '8rem' }}>
-        {
-          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(i => (
-            <Grid key={i} xs={6}>
-              <CardItem command='Git push texto de prueba pa ver esta el largo del componente
-              '/>
-            </Grid>
-          ))
-        }
+      <Grid.Container
+        gap={1}
+        css={{ marginBottom: '8rem' }}
+      >
+        {array.map((i) => (
+          <Grid key={i} xs={6}>
+            <CardItem
+              command='Git push texto de prueba pa ver esta el largo del componente
+              '
+            />
+          </Grid>
+        ))}
       </Grid.Container>
     </Container>
   )
--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import Image from "next/image"
 import { Navbar } from "components/navbar"
 import { Container } from "@nextui-org/react"
 
