b7285d1064f629e4b8cf57063ef36d1a1b432a2f
Beto Toro <beto.toro@zemoga.com>
2022-03-02 16:51:23 -0500

    [FEAT]: separate the prisma configuration into a single file and crate and endpoint that give us all users

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,7 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { prisma } from 'prisma/index'
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -12,5 +10,6 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     })
-  ]
+  ],
+  secret: process.env.SECRET
 })
