6438a5f3ee6a92f9f2087aa37021ddb6fcda97d4
Beto Toro <betotoro0902@gmail.com>
2022-03-11 11:42:26 -0500

    [FEAT]: create useForm custom hook

---
+++ hooks/useForm.tsx
@@ -0,0 +1,18 @@
+import { useCallback, useContext } from 'react';
+import valuesContext, { CommandValues } from 'context/valuesContext'
+
+export const useForm = (): [any, (attribute: string, content: any) => void] => {
+  const { command, setCommand } = useContext(valuesContext)
+
+  const handleCommand = useCallback((attribute, content): void => {
+    setCommand((prevState) => {
+      return {
+        ...prevState,
+        [attribute]: content,
+      }
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return [command, handleCommand]
+}
